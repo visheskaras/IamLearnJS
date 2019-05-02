e0e41b47d26e4a499301a4c791e48cdb5e15a455
Visheskaras <kain.shiba@gmail.com>
2019-05-02 20:44:28 +0300

    second

---
+++ js.js
@@ -0,0 +1,12 @@
+"use strict";
+let money, time;
+money = + prompt("Ваш бюджет на месяц?","0");
+time = prompt("Введите дату в формате YYYY-MM-DD","");
+let appData = {
+    money: money,
+    timeData: time,
+    expenses,
+    optinalExpanses,
+    income,
+    saving: false
+};
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+var a = 199;
+
+a == 200 ? console.log("=200") : console.log("not = 200");
+
+switch (a) {
+    case a < 99:
+        console.log("<99");
+        break;
+    case a > 100:
+        console.log("ok");
+        break;
+}

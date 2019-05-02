76276b9aac91cce63ff934b3a015c410e416843a
Visheskaras <kain.shiba@gmail.com>
2019-05-02 20:55:05 +0300

    new

--- js.js
+++ js.js
@@ -7,6 +7,14 @@ let appData = {
     timeData: time,
     expenses,
     optinalExpanses,
-    income,
+    income: [],
     saving: false
 };
+
+appData.expenses[0].article = prompt("Введите обязательную статью расходов в этом месяц","");
+appData.expenses[0].money = +prompt("Во сколько обойдется?","0");
+
+appData.expenses[1].article = prompt("Введите обязательную статью расходов в этом месяц","");
+appData.expenses[1].money = +prompt("Во сколько обойдется?","0");
+
+alert(appData.money-appData.expenses[0].money-appData.expenses[1].money);
